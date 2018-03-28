32bf079cbd44a24d368001f34baf652e5c8a96af
sglondon <susangs2@gmail.com>
2018-03-28 11:25:33 -0400

    added chat controller

--- ChatterEx/AspNetUserClaim.cs
+++ ChatterEx/AspNetUserClaim.cs
@@ -12,12 +12,12 @@ namespace ChatterEx
     using System;
     using System.Collections.Generic;
     
-    public partial class Chat
+    public partial class AspNetUserClaim
     {
-        public int ID { get; set; }
-        public string UserID { get; set; }
-        public System.DateTime TimeStamp { get; set; }
-        public string Message { get; set; }
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string ClaimType { get; set; }
+        public string ClaimValue { get; set; }
     
         public virtual AspNetUser AspNetUser { get; set; }
     }
--- ChatterEx/AspNetUserLogin.cs
+++ ChatterEx/AspNetUserLogin.cs
@@ -12,12 +12,11 @@ namespace ChatterEx
     using System;
     using System.Collections.Generic;
     
-    public partial class Chat
+    public partial class AspNetUserLogin
     {
-        public int ID { get; set; }
-        public string UserID { get; set; }
-        public System.DateTime TimeStamp { get; set; }
-        public string Message { get; set; }
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+        public string UserId { get; set; }
     
         public virtual AspNetUser AspNetUser { get; set; }
     }
